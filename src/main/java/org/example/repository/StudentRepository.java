85968b1892228c3bd3e0eed76ac0cfcbb586566b
sokny2023 <psnpsn667788@gmail.com>
2024-06-18 23:33:18 +0700

    updated report

--- src/main/java/org/example/repository/StudentRepository.java
+++ src/main/java/org/example/repository/StudentRepository.java
@@ -1,11 +1,7 @@
 package org.example.repository;
 
-import org.example.model.Report;
-import org.example.util.PropertyLoader;
 
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class StudentRepository {
 
